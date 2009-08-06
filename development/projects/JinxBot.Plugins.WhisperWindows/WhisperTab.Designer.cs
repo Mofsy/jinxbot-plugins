3558c32d272720cef3194c07d99f82848dd63fd3
myndfyre <myndfyre@valhallalegends.com>
2009-08-06 15:18:25 +0000

    Implemented a sample whisper windows plugin

---
+++ development/projects/JinxBot.Plugins.WhisperWindows/WhisperTab.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace JinxBot.Plugins.WhisperWindows
+{
+    partial class WhisperTab
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chatBox1 = new JinxBot.Controls.ChatBox();
+            this.SuspendLayout();
+            // 
+            // chatBox1
+            // 
+            this.chatBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chatBox1.Location = new System.Drawing.Point(0, 0);
+            this.chatBox1.Name = "chatBox1";
+            this.chatBox1.Size = new System.Drawing.Size(666, 392);
+            this.chatBox1.TabIndex = 0;
+            this.chatBox1.DisplayReady += new System.EventHandler(this.chatBox1_DisplayReady);
+            this.chatBox1.MessageReady += new JinxBot.Controls.MessageEventHandler(this.chatBox1_MessageReady);
+            // 
+            // WhisperTab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 392);
+            this.Controls.Add(this.chatBox1);
+            this.Name = "WhisperTab";
+            this.TabText = "WhisperTab";
+            this.Text = "WhisperTab";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private JinxBot.Controls.ChatBox chatBox1;
+    }
+}
---
+++ development/projects/JinxBot.Plugins.WhisperWindows/WhisperWindowsPlugin.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JinxBot.Plugins.UI;
+using System.Windows.Forms;
+using BNSharp;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace JinxBot.Plugins.WhisperWindows
+{
+    [JinxBotPlugin(
+        Author = "MyndFyre",
+        Name = "JinxBot Whisper Windows Plugin",
+        Url = "http://www.jinxbot.net/wiki/Whisper_Window_Plugin",
+        Version = "1.0.0.0",
+        Description = "Pops up whisper windows for whisper conversations.")]
+    internal class WhisperWindowsPlugin : ISingleClientPlugin
+    {
+        private delegate void Invokee();
+
+        private IJinxBotClient m_client;
+        private IProfileDocument m_profileDoc;
+        private Dictionary<string, WhisperTab> m_forms = new Dictionary<string, WhisperTab>();
+        
+        #region ISingleClientPlugin Members
+
+        public void CreatePluginWindows(JinxBot.Plugins.UI.IProfileDocument profileDocument)
+        {
+            m_profileDoc = profileDocument;
+        }
+
+        public void DestroyPluginWindows(JinxBot.Plugins.UI.IProfileDocument profileDocument)
+        {
+            lock (m_forms)
+            {
+                foreach (string key in m_forms.Keys)
+                {
+                    WhisperTab tab = m_forms[key];
+                    tab.Close();
+                    tab.Dispose();
+                }
+                m_forms.Clear();
+            }
+        }
+
+        public void RegisterEvents(IJinxBotClient profileClient)
+        {
+            m_client = profileClient;
+
+            m_client.Client.RegisterWhisperReceivedNotification(Priority.Low, client_WhisperReceived);
+            m_client.Client.RegisterWhisperSentNotification(Priority.Low, client_WhisperSent);
+        }
+
+        public void UnregisterEvents(IJinxBotClient profileClient)
+        {
+            m_client.Client.UnregisterWhisperReceivedNotification(Priority.Low, client_WhisperReceived);
+            m_client.Client.UnregisterWhisperSentNotification(Priority.Low, client_WhisperSent);
+        }
+
+        #endregion
+
+        private void client_WhisperReceived(object sender, ChatMessageEventArgs e)
+        {
+            Invokee del = delegate
+            {
+                lock (m_forms)
+                {
+                    WhisperTab tab = null;
+                    if (!m_forms.ContainsKey(e.Username))
+                    {
+                        tab = new WhisperTab(m_client);
+                        tab.OtherPerson = e.Username;
+                        tab.MyUsername = m_client.Client.UniqueUsername;
+                        m_profileDoc.AddDocument(tab);
+                        tab.TabText = tab.Text = e.Username;
+                        tab.Disposed += tab_Disposed;
+
+
+                        m_forms.Add(e.Username, tab);
+                    }
+                    else
+                    {
+                        tab = m_forms[e.Username];
+                    }
+
+                    tab.AddReceived(e);
+                }
+            };
+            ISynchronizeInvoke sync = m_profileDoc as ISynchronizeInvoke;
+            if (sync.InvokeRequired)
+                sync.BeginInvoke(del, new object[0]);
+            else
+                del();
+        }
+
+        private void tab_Disposed(object sender, EventArgs e)
+        {
+            lock (m_forms)
+            {
+                WhisperTab tab = (WhisperTab)sender;
+                if (m_forms.ContainsKey(tab.OtherPerson))
+                {
+                    m_forms.Remove(tab.OtherPerson);
+                }
+                tab.Disposed -= tab_Disposed;
+            }
+        }
+
+        private void client_WhisperSent(object sender, ChatMessageEventArgs e)
+        {
+            Invokee del = delegate
+            {
+                lock (m_forms)
+                {
+                    WhisperTab tab = null;
+                    if (!m_forms.ContainsKey(e.Username))
+                    {
+                        tab = new WhisperTab(m_client);
+                        tab.OtherPerson = e.Username;
+                        tab.MyUsername = m_client.Client.UniqueUsername;
+                        m_profileDoc.AddDocument(tab);
+                        tab.TabText = tab.Text = e.Username;
+                        tab.Disposed += tab_Disposed;
+
+                        m_forms.Add(e.Username, tab);
+                    }
+                    else
+                    {
+                        tab = m_forms[e.Username];
+                    }
+
+                    tab.AddSent(e);
+                }
+            };
+            ISynchronizeInvoke sync = m_profileDoc as ISynchronizeInvoke;
+            if (sync.InvokeRequired)
+                sync.BeginInvoke(del, new object[0]);
+            else
+                del();
+        }
+
+        #region IJinxBotPlugin Members
+
+        public void Startup(IDictionary<string, string> settings)
+        {
+            
+        }
+
+        public void Shutdown(IDictionary<string, string> settings)
+        {
+            
+        }
+
+        public object GetSettingsObject()
+        {
+            return new object();
+        }
+
+        public IPluginUpdateManifest CheckForUpdates()
+        {
+            return null;
+        }
+
+        #endregion
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            
+        }
+
+        #endregion
+    }
+}
